890287360f5cb2c1d81c8f0df763cd3341621682
Milena81 <milena_81@mail.bg>
2016-12-03 11:01:08 +0200

    Added Footer component
    Added Footer.js component and some css with status /* TODO */

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 3.12.2016 г..
+ */
+import React, {Component} from 'react';
+import './Footer.css';
+export default class Footer extends Component {
+    render(){
+        return (
+            <footer className="footer"> &copy; TeamProject-JSApplications</footer>
+        );
+    }
+}
