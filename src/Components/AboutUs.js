ccf5c2f923ec591047de4994bbb26c4455613dc2
Milena81 <milena_81@mail.bg>
2016-12-04 09:29:40 +0200

    added AboutUs.js file
    added AboutUs.js file

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Milena on 4.12.2016 г..
+ */
+import React, {Component} from 'react';
+
+export default class About extends Component{
+    render() {
+        <div>
+            <h1>Ubout Us</h1>
+            <p> some text...</p>
+        </div>
+    }
+}
